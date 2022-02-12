eee0772e39de94e0f9d11462abc3b0243246e9a6
N taro <taro@Taros-MacBook-Pro.local>
2022-02-12 16:31:49 +0900

    Thymeleafでhtmlを作成

--- hello-world/src/main/java/org/tkr/app/HomeController.java
+++ hello-world/src/main/java/org/tkr/app/HomeController.java
@@ -1,12 +1,20 @@
 package org.tkr.app;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController{
 	@RequestMapping("/") 
 	public static String home() {
 		return "Hello World!";
 	}
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+		String name = "Taro";
+		model.addAttribute("name", name);
+		return "helloworld";
+	}
+	
 }
